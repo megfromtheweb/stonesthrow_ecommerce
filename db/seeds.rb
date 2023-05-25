09b562f29d699f35eaf5b1080fa68fb6d29c6fe1
Meg Goffin <megan.goffin@bt.com>
2023-05-25 20:24:07 +0100

    Improve styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-5.times do |_i|
-  Product.create(name: "Product", sku: "abc123", price: 2)
+20.times do |i|
+  Product.create(name: "Product #{i}", sku: "ST00#{i}", price: 2.00)
 end
